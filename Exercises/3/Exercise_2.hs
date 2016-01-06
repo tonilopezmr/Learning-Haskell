da74fd4e8e9e798dc0aa3c3bd3c24da4155c7239
Antonio López Marín <tonilopezmr@gmail.com>
2016-01-06 20:49:45 +0100

    Modify exercise_2.hs

--- Exercises/3/Exercise_2.hs
+++ Exercises/3/Exercise_2.hs
@@ -1,5 +1,6 @@
-(==) :: (Stack a) => [a] -> [a] -> Bool
-[]     == []     = True
-[]     == (x:xs) = False
-(x:xs) == []     = False
-(x:xs) == (y:ys) = x==y && xs==ys
+--in Stack.hs
+instance (Eq a) => Eq (Stack a) where
+	EmptyStack  == 	EmptyStack 	= True
+	EmptyStack 	== 	(Stk x s) 	= False
+	(Stk x s) 	== 	EmptyStack 	= False	
+	(Stk x s) 	== 	(Stk y z) 	= x==y && s==z
